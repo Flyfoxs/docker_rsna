6ba12252385282e7c21586814085b3f31c9335a7
flyfoxs <lilao@163.com>
2020-07-10 20:50:18 +0800

    share storage

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo $cur_path
 
 mkdir -p /tmp/docker
 
-docker_name=gpu_cuda10
+docker_name=share_cuda10
 docker stop $docker_name
 docker rm $docker_name
 
@@ -15,8 +15,7 @@ docker run  -dit \
                --gpus all \
                -v /share/data/lung/RSNA:/input \
                -v /home/felix/.jupyter:/root/.jupyter \
-	       -v /home/felix/pj/rsna-pneumonia:/app \
-               -v /share/data/lung/RSNA:/app/data \
-		-v /tmp/docker:/tmp \
-               --name $docker_name  -p 9988:8888 $docker_name
+	       -v /home/docker_user:/app \
+	      -v /tmp/docker:/tmp \
+               --name $docker_name  -p 8885:8888 $docker_name
 # -v /home/felix/pj/detectron2:/home/appuser/detectron2_repo \
